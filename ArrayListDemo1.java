325839885a73399ebfb38c38a038bb961ab93a3e
stan870224 <stan870224@gmail.com>
2023-08-03 16:13:20 +0800

    commit20230803

---
+++ ArrayListDemo1.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo1{
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList();
+        list.add("heima");
+        list.add("666");
+        System.out.println(list);
+        
+    }
+}
